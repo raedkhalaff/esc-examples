deb34100ac07c285a61e70e2349d4b5404c27d4a
Komal <komal@pulumi.com>
2024-07-21 10:31:36 -0700

    Remove usused code

--- sync/aws-secretsmanager/index.ts
+++ sync/aws-secretsmanager/index.ts
@@ -1,10 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as fs from "fs";
 
 // Read a json file from the local filesystem using node.js fs module
 const json = fs.readFileSync("sync.json", "utf8");
-const openedEnv = JSON.parse(json);
 
 // Create an AWS resource (S3 Bucket)
 const secret = new aws.secretsmanager.Secret("mySecretSyncedFromESC", {
