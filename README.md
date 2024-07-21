0e9c6b5a637f56c8ae116646a11d2338814fab42
Komal <komal@pulumi.com>
2024-07-20 21:22:55 -0700

    Add WIP sync example

---
+++ README.md
@@ -0,0 +1,7 @@
+# ESC Examples
+
+[Pulumi ESC](https://www.pulumi.com/product/esc/) (Environments, Secrets and Configuration), is an open source project and managed service of Pulumi Cloud that enables teams to manage hierarchical collections of configuration and secrets and consume them from a variety of different infrastructure and application services.
+
+Pulumi ESC integrates with Pulumi Cloud identity and RBAC to provide rich control over access to secret configuration within an organization. Pulumi ESC supports multiple configuration providers, enabling static key/value configuration as well as dynamically retrieved configuration and secrets via OIDC and additional providers like 1Password and Vault.  Pulumi ESC is available via the new esc CLI, Pulumi Cloud, the Pulumi Cloud REST API, and Pulumi IaC stack configuration.
+
+This repo contains examples of various ESC use-cases.
---
+++ sync/index.ts
@@ -0,0 +1,54 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as service from "@pulumi/pulumiservice";
+
+const orgName = "pulumi";
+
+const env = new service.Environment("env", {
+    organization: orgName,
+    name: "esc-sync-aws-secretsmanager",
+  yaml: new pulumi.asset.StringAsset(
+`imports:
+  - dev-sandbox
+values:
+  myConfigKey: myImportantValue
+  myNestedKey:
+    haha: business
+  mySecret: \${aws.login.accessKeyId}}
+`
+  )
+})
+
+const stack = new service.Stack("esc-sync-aws-secretsmanager", {
+    organizationName: orgName,
+    projectName: "esc-sync-aws-secretsmanager",
+    stackName: "dev",
+})
+
+const settings = new service.DeploymentSettings("deployment_settings", {
+    organization: orgName,
+    project: stack.projectName,
+    stack: stack.stackName,
+    github: {
+        repository: "pulumi/esc-examples",
+    },
+    sourceContext: {
+        git: {
+            branch: "main",
+            repoDir: "sync/aws-secretsmanager",
+        }
+    },
+    operationContext: {
+        preRunCommands: [
+            `pulumi config env add ${env.name}}`
+        ]
+    }
+});
+
+const schedule = new service.DeploymentSchedule("schedule", {
+    organization: orgName,
+    project: settings.project,
+    stack: settings.stack,
+    scheduleCron: "*/30 * * * *",
+    pulumiOperation: "update",
+})
+
