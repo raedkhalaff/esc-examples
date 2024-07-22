3a3f9af479f4c60d15116b44531c579f227f2ac3
Komal <komal@pulumi.com>
2024-07-21 22:15:35 -0700

    parameterize

--- sync/index.ts
+++ sync/index.ts
@@ -1,57 +1,60 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as service from "@pulumi/pulumiservice";
+import * as fs from "fs";
 
-const orgName = "pulumi";
+const config = new pulumi.Config();
+const orgName = config.require("orgName");
+const projectName = config.require("projectName");
+const stackName = config.require("stackName");
+const repository = config.require("repository");
+const syncCronSchedule = config.get("syncCronSchedule") || "0 * * * *" // default to hourly;
+const envPath = config.get("envPath") || "syncEnv.yaml";
+
+const envContent = fs.readFileSync(envPath, "utf8");
 
 const env = new service.Environment("env", {
-    organization: orgName,
-    name: "esc-sync-aws-secretsmanager",
-  yaml: new pulumi.asset.StringAsset(
-`imports:
-  - dev-sandbox
-values:
-  mySyncedKey:
-    myConfigKey: myImportantValue
-    myNestedKey:
-      haha: business
-    mySecret: \${aws.login.accessKeyId}
-`
-  )
-})
+  organization: orgName,
+  name: `${projectName}-${stackName}`,
+  yaml: new pulumi.asset.StringAsset(envContent)
+});
 
 const stack = new service.Stack("esc-sync-aws-secretsmanager", {
     organizationName: orgName,
-    projectName: "esc-sync-aws-secretsmanager",
-    stackName: "dev",
+    projectName,
+    stackName,
 })
+
+const fullyQualifiedStackName = pulumi.interpolate`${orgName}/${projectName}/${stackName}`;
+const fullyQualifiedEnvName = pulumi.interpolate`${orgName}/${env.name}`;
 
 const settings = new service.DeploymentSettings("deployment_settings", {
     organization: orgName,
     project: stack.projectName,
     stack: stack.stackName,
     github: {
-        repository: "pulumi/esc-examples",
+        repository,
     },
     sourceContext: {
         git: {
             branch: "main",
-            repoDir: "sync/aws-secretsmanager",
+            repoDir: "sync/target",
         }
     },
     operationContext: {
         preRunCommands: [
             'pulumi login',
-            pulumi.interpolate`pulumi config env add ${env.name} -s ${orgName}/${stack.projectName}/${stack.stackName} --yes`,
-            pulumi.interpolate`pulumi env open ${orgName}/${env.name} mySyncedKey > sync.json`,
+            pulumi.interpolate`pulumi config env add ${env.name} -s ${fullyQualifiedStackName} --yes`,
+            pulumi.interpolate`pulumi env open ${fullyQualifiedEnvName} sync.awsSecretsManager.value > sync.json`,
+            pulumi.interpolate`pulumi config set secretName $(pulumi env open ${fullyQualifiedEnvName} sync.awsSecretsManager.name)`,
         ]
     }
 });
 
-const schedule = new service.DeploymentSchedule("schedule", {
+const schedule = new service.DeploymentSchedule("update_schedule", {
     organization: orgName,
     project: settings.project,
     stack: settings.stack,
-    scheduleCron: "0 * * * *",
+    scheduleCron: syncCronSchedule,
     pulumiOperation: "update",
 })
 
--- sync/target/index.ts
+++ sync/target/index.ts
@@ -1,14 +1,18 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as fs from "fs";
+
+const config = new pulumi.Config();
+const name = config.require("secretName");
 
 // Read a json file from the local filesystem using node.js fs module
 const json = fs.readFileSync("sync.json", "utf8");
 
-const secret = new aws.secretsmanager.Secret("mySecretSyncedFromESC", {
+const secret = new aws.secretsmanager.Secret(name, {
     recoveryWindowInDays: 0,
 })
 
-const secretVersion = new aws.secretsmanager.SecretVersion("mySecretSyncedFromESCVersion", {
+const secretVersion = new aws.secretsmanager.SecretVersion(`${name}-version`, {
     secretId: secret.id,
     secretString: json,
 })
