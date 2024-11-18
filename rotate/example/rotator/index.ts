f70bd178dcddd2815d627a18a6ca27c785144d05
Claire Gaestel <213631+nyobe@users.noreply.github.com>
2024-11-17 17:19:32 -0800

    example rotator fixup fixup afsd

---
+++ rotate/example/rotator/index.ts
@@ -0,0 +1,24 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+import {Rotator} from "./rotator";
+
+const creds = new Rotator("rotating-creds", {
+    // trigger credential rotation every deployment.
+    // (the scheduler stack will create a deployment schedule for this stack to effect the rotation)
+    trigger: Date().toString(),
+    // create a set of equivalent credentials that will be rotated between.
+    // a particular credential should be replaced whenever `trigger` is updated.
+    construct: (name, trigger) => {
+        // in this example we're just creating passwords,
+        // but these could be mysql users or anything else.
+        return new random.RandomPassword(name, {
+            length: 10,
+            keepers: {trigger}
+        })
+    },
+})
+
+// export the currently active credential, which will be imported by the downstream ESC environment.
+export const current = creds.current.result;
+// previous represents the inactive credential that is still valid, but will be replaced next rotation.
+export const previous = creds.previous.result;
